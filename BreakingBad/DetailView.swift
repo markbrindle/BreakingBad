069aee982540b4773105b7f65d87556c664f5e71
Mark Brindle <mark.brindle@arkemm.com>
2020-09-10 22:57:55 +0100

    Split DetailView into its own file

--- BreakingBad/DetailView.swift
+++ BreakingBad/DetailView.swift
@@ -1,53 +1,55 @@
 //
-//  ContentView.swift
+//  DetailView.swift
 //  BreakingBad
 //
-//  Created by Mark Brindle on 24/08/2020.
+//  Created by Mark Brindle on 10/09/2020.
 //  Copyright © 2020 ARKEMM Software Limited. All rights reserved.
 //
 
 import SwiftUI
 
-private let dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .medium
-    dateFormatter.timeStyle = .medium
-    return dateFormatter
-}()
+struct DetailView: View {
 
-struct ContentView: View {
-    @State private var dates = [Date]()
+    @EnvironmentObject var controller: CharactersController
     
-    @ObservedObject var controller: CharactersController
+    @Binding var selectedCharacterId: String?
     
-    @State private var selectedCharacterId: String?
-
     var body: some View {
-        NavigationView {
-            MasterView(selectedCharacterId: $selectedCharacterId)
-                .navigationBarTitle(Text("Characters"))
-            DetailView(selectedCharacterId: $selectedCharacterId)
+        Group {
+            viewForCharacter(selectedCharacterId: selectedCharacterId)
+        }.navigationBarTitle(Text("Detail"), displayMode: .inline)
+    }
+    
+    private func viewForCharacter(selectedCharacterId: String?) -> Text {
+        if let character = controller.characters.first(where: { "\($0.char_id)" == self.selectedCharacterId }) {
+            return Text(character.name)
+        } else {
+            return Text("Unknown")
         }
-        .onAppear(perform: controller.loadData)
-        .navigationViewStyle(DoubleColumnNavigationViewStyle())
-        .environmentObject(controller)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+#if DEBUG
+struct DevailView_Previews: PreviewProvider {
     static let characters = [
         BBCharacter(char_id: 1, name: "Walter White", birthday: "01 01 1970", occupations: ["High School Chemistry Teacher", "Meth King Pin"], status: "Presumed dead", nickname: "Heisenberg", imageURL: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg", appearances: [1,2,3,4,5] ),
-
+        
         BBCharacter(char_id: 2, name: "Jesse Pinkman", birthday: "09-24-1984", occupations: ["Meth Dealer"], status: "Alive", nickname: "Cap n' Cook", imageURL: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Jesse_Pinkman2.jpg/220px-Jesse_Pinkman2.jpg", appearances: [1, 2, 3, 4, 5] ),
-
+        
         BBCharacter(char_id: 11, name: "Domingo Molina", birthday: "Unknown", occupations: ["Meth Distributor"], status: "Deceased", nickname: "Krazy-8", imageURL: "https://vignette.wikia.nocookie.net/breakingbad/images/e/e7/Krazy8.png/revision/latest?cb=20130208041554", appearances: [1] ),
     ]
-
+    
     static var previews: some View {
         Group {
-            ContentView(controller: CharactersController(_characters: Published(initialValue: characters)))
-            ContentView(controller: CharactersController(_characters: Published(initialValue: characters)))
-                .environment(\.colorScheme, .dark)
+            NavigationView {
+                MasterView(selectedCharacterId: .mock("11"))
+            }.environmentObject(CharactersController(_characters: Published(initialValue: characters)))
+            NavigationView {
+                MasterView(selectedCharacterId: .mock("2"))
+                    .environment(\.colorScheme, .dark)
+            }.environmentObject(CharactersController(_characters: Published(initialValue: characters)))
         }
     }
 }
+#endif
+
